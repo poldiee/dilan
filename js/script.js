8cc0931e1fe82c2598933344c537dc0415233804
poldiee <nessidave@gmail.com>
2022-03-13 12:45:07 +0300

    checks on jQuery functions

--- js/script.js
+++ js/script.js
@@ -83,19 +83,19 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){
-  $("form#form34A").submit(function(event){
-    // event.preventDefault();
-    var name = $("input#MERGE1").val();
-    var email = $("input#MERGE0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-      alert (name + ", we have received your message. Thank you for reaching out to us.");
-    }
-    else {
-      alert("Please enter your name and email!");
-    }
+// $(document).ready(function(){
+//   $("form#form34A").submit(function(event){
+//     // event.preventDefault();
+//     var name = $("input#MERGE1").val();
+//     var email = $("input#MERGE0").val();
+//     var message = $("textarea#comment").val();
+//     if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+//       alert (name + ", we have received your message. Thank you for reaching out to us.");
+//     }
+//     else {
+//       alert("Please enter your name and email!");
+//     }
     
-  });
+//   });
 
-});
+// });
