acee3b9de80afb14df1f4f1d8dd45bfa431a0a89
poldiee <nessidave@gmail.com>
2022-03-13 13:03:56 +0300

    form submit jQuery

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ $(document).ready(function(){
     var email = $("input#MERGE0").val();
     var message = $("textarea#comment").val();
     if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-      alert (name + ", we have received your message. Thank you for reaching out to us.");
+      alert (name + ",your message received. Thank you.");
     }
     else {
       alert("Please enter your name and email!");
